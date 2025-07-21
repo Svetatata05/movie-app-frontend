4379dc63fb8cc76f59ad39d8b01fb02af8951995
Svetatata05 <svitlana.koturlash-ip221@nung.edu.ua>
2025-07-21 20:18:03 +0300

    Delete src directory

--- src/features/counter/Counter.module.css.d.ts
+++
@@ -1,9 +0,0 @@
-declare const styles: {
-    row: string;
-    button: string;
-    value: string;
-    textbox: string;
-    asyncButton: string;
-  };
-  
-  export default styles;
--- src/features/movies/selectors.ts
+++
@@ -1,7 +0,0 @@
-import { RootState } from '../../app/store';
-import { Movie } from './types';
-
-export const selectMovies = (state: RootState): Movie[] => {
- 
-  return state.movies.movies;
-};
